0bda2a5ac7d12ad792a123f82c3d6366c5ec44f5
Jackson Walker <cheezcakep@gmail.com>
2016-05-13 15:33:39 -0500

    Added frames
    help I suck at programming

--- BeepBoopTranslator.py
+++ BeepBoopTranslator.py
@@ -1,8 +1,18 @@
 from tkinter import *
 
 class mainWindows:
-    def __init__(self, w):
-        self.w = w
+    def __init__(self, master):
+        self.m = master
+        Frame.__init__(self, master)
+        self.topFrame = Frame(self.m)
+        self.middleFrame = Frame(self.m)
+        self.midLeftFrame = Frame(self.m)
+        self.bottomFrame = Frame(self.m)
+        self.topFrame.pack(Side=TOP)
+        self.middleFrame.pack()
+        self.midLeftFrame.pack(Side=LEFT)
+        self.bottomFrame.pack(Side=BOTTOM)
+
 
 if __name__=='__main__':
     root = Tk()
@@ -14,7 +24,7 @@ if __name__=='__main__':
     root.title('Beep Boop Translation')
     root.geometry('%dx%d+%d+%d' % (w, h, ((sw - w)/2), ((sh - h)/2)))
     root.resizable(0,0)
-    root.configure(background='Red')
+    root.configure(background='Grey')
     #root.iconbitmap(r'translate.ico')
     root.focus_set()
 
